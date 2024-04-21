11da82cd6d790d9dc3f408cb42ceb11634eb46e4
erreep <59733316+erreep@users.noreply.github.com>
2024-04-21 20:42:12 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import openai 
+import os
+import dotenv
+
+# Load environment variables
+dotenv.load_dotenv()
+
+#import with dotenc
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+print(GROQ_API_KEY)
